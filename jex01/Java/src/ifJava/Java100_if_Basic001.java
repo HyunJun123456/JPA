b6c32872ae2c5c1d7bfde961e4e0da8b67ecb987
HyunJun123456 <juni303@chauniv.ac.kr>
2022-10-14 22:53:54 +0900

    입출력

---
+++ jex01/Java/src/ifJava/Java100_if_Basic001.java
@@ -0,0 +1,18 @@
+package ifJava;
+
+public class Java100_if_Basic001 {
+	public static void main(String[] args) {
+		// 1. 변수 선언
+		int kor=70, eng=90, math=100;
+		
+		//2. if 조건문만 사용하기
+		if(kor >= 80) {
+			System.out.println("당신의 국어 점수는 B학점입니다.");
+		}
+		System.out.println("학점이 안나왔다면 80점이 안되서입니다.");
+		
+	}
+	
+	
+	
+}
